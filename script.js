3eb3f036fa21eeb915b97d2956ba8aceab83db3b
BistonN <biston.nunes@gmail.com>
2026-03-13 14:41:48 -0300

    alterando rota da api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* --- DADOS DAS PROVAS --- */
-const API_URL = 'http://10.91.249.10:3069';
+const API_URL = 'http://' + window.location.hostname + ':3069';;
 const TEMPO_PROVA_MIN = 40;
 let provaAtual = [];
 let indiceQuestao = 0;
